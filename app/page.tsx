5fd8a350751892d436c3194ffbb529feee4d3d47
ElormCodes1 <marriondokosi@gmail.com>
2023-07-09 21:00:35 +0000

    create mobile view of first frontend mentor project

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,85 @@
-import Image from 'next/image'
+import {PiLightningDuotone} from 'react-icons/pi'
+import {HiOutlineClock} from "react-icons/hi";
+import {AiOutlineMessage} from "react-icons/ai";
+import {FiEye} from "react-icons/fi";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="bg-white">
+          <div className="rounded-b-3xl bg-gradient-to-b from-[#7115cc] to-[#3201b8] h-[400px] w-[400px] flex flex-col items-center">
+              <p className="py-8 text-xl">
+                  Your Result
+              </p>
+              <div className="flex flex-col justify-center items-center bg-gradient-to-b from[#a371d6] to-[#34148e] rounded-full h-[150px] w-[150px]">
+                  <span className="text-6xl font-bold">76</span>
+                  <p className="text-sm opacity-40">
+                      of 100
+                  </p>
+              </div>
+              <p className="py-5 text-xl font-bold">
+                  Great
+              </p>
+              <p className="text-justify text-sm opacity-70">
+                  <span>You scored higher than 65% of</span> <span>the <br/> people who have taken</span> <span>these
+                  tests.</span>
+              </p>
+          </div>
+          <div className="bg-white w-[400px] h-[500px] flex flex-col my-5">
+              <div className="mx-5">
+                  <p className="text-black ml-1 mb-3 text-lg font-semibold">
+                      Summary
+                  </p>
+                  <div className="bg-red-100 flex rounded-2xl h-[60px] p-2 items-center">
+                      <div className="w-1/2 flex items-center">
+                          <PiLightningDuotone className="mx-2 text-red-500"/>
+                          <span className="text-red-500">Reaction</span>
+                      </div>
+                      <div className="w-1/2 flex justify-end mr-2">
+                          <span className="text-black mr-1">80</span>
+                          <span className="text-black opacity-50 ml-1">/  100</span>
+                      </div>
+                  </div>
+                  <br/>
+                  <div className="bg-yellow-100 flex rounded-2xl h-[60px] p-2 items-center">
+                      <div className="w-1/2 flex items-center">
+                          <HiOutlineClock className="mx-2 text-yellow-500"/>
+                          <span className="text-yellow-500">Memory</span>
+                      </div>
+                      <div className="w-1/2 flex justify-end mr-2">
+                          <span className="text-black mr-1">92</span>
+                          <span className="text-black opacity-50 ml-1">/  100</span>
+                      </div>
+                  </div>
+                  <br/>
+                  <div className="bg-green-100 flex rounded-2xl h-[60px] p-2 items-center">
+                      <div className="w-1/2 flex items-center">
+                          <AiOutlineMessage className="mx-2 text-green-500"/>
+                          <span className="text-green-500">Verbal</span>
+                      </div>
+                      <div className="w-1/2 flex justify-end mr-2">
+                          <span className="text-black mr-1">61</span>
+                          <span className="text-black opacity-50 ml-1">/  100</span>
+                      </div>
+                  </div>
+                  <br/>
+                  <div className="bg-indigo-100 flex rounded-2xl h-[60px] p-2 items-center">
+                      <div className="w-1/2 flex items-center">
+                          <FiEye className="mx-2 text-indigo-500"/>
+                          <span className="text-indigo-500">Visual</span>
+                      </div>
+                      <div className="w-1/2 flex justify-end mr-2">
+                          <span className="text-black mr-1">72</span>
+                          <span className="text-black opacity-50 ml-1">/  100</span>
+                      </div>
+                  </div>
+                  <div className="flex items-center justify-center m-5 mt-10">
+                      <button className="bg-black h-[60px] w-[500px] opacity-80 rounded-full">
+                          Continue
+                      </button>
+                  </div>
+              </div>
+          </div>
       </div>
     </main>
   )
