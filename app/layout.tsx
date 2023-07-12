28514927d0ef94ba666773fe3190601d5b625caa
ElormCodes1 <marriondokosi@gmail.com>
2023-07-12 18:28:25 +0000

    uncomment tracking link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,32 +17,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        {/*<head>*/}
-        {/*  <script async src="https://www.googletagmanager.com/gtag/js?id=G-5WJ0EM90VM"></script>*/}
-        {/*  <script*/}
-        {/*      dangerouslySetInnerHTML={{*/}
-        {/*        __html: `*/}
-        {/*        window.dataLayer = window.dataLayer || [];*/}
-        {/*        function gtag(){dataLayer.push(arguments);}*/}
-        {/*        gtag('js', new Date());*/}
-        {/*        gtag('config', 'G-BGN4MXF8YW');*/}
-        {/*      `,*/}
-        {/*      }}*/}
-        {/*  />*/}
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-5WJ0EM90VM"></script>
+          <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-BGN4MXF8YW');
+              `,
+              }}
+          />
 
-        {/*  <script*/}
-        {/*      dangerouslySetInnerHTML={*/}
-        {/*        {*/}
-        {/*          __html: `*/}
-        {/*      (function(c,l,a,r,i,t,y){*/}
-        {/*        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};*/}
-        {/*        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;*/}
-        {/*        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);*/}
-        {/*    })(window, document, "clarity", "script", "hy5lx23rap");`,*/}
-        {/*        }}*/}
-        {/*  />*/}
-        {/*  <title> Hello world</title>*/}
-        {/*</head>*/}
+          <script
+              dangerouslySetInnerHTML={
+                {
+                  __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "hy5lx23rap");`,
+                }}
+          />
+          <title> Hello world</title>
+        </Head>
       <body className={inter.className}>
         {children}
       <Analytics />
