5684b017fe5d1795346c4db56f8f8e2b8ab0efc9
ElormCodes1 <marriondokosi@gmail.com>
2023-07-12 15:22:11 +0000

    add ms clarity and google analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-5WJ0EM90VM"></script>
+          <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-BGN4MXF8YW');
+              `,
+              }}
+          />
+
+          <script
+              dangerouslySetInnerHTML={
+                {
+                  __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "hy5lx23rap");`,
+                }}
+          />
+          <title> Hello world</title>
+        </head>
       <body className={inter.className}>
         {children}
       <Analytics />
