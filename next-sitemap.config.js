02739786be668511ae48b8c3bbd52efd3228c7e8
Elorm Marrion Dokosi <marriondokosi@gmail.com>
2023-07-31 17:00:46 +0000

    add sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://elormslearningjourney.tech',
-    generateRobotsTxt: true,
-    // optional
-    robotsTxtOptions: {
-        policies: [{ userAgent: '*', allow: '/' }],
-      },
-  }
+    siteUrl: 'https://elormslearningjourney.tech', // Replace this with your actual domain name
+    generateRobotsTxt: true, // This will generate a robots.txt as well
+    // Other configuration options can be added here, if needed.
+  };
+  
