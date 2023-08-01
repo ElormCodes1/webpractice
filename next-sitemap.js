bc5bfc1cc4308a59086c3dec7951b2a3aacaf14d
Elorm Marrion Dokosi <marriondokosi@gmail.com>
2023-08-01 13:38:35 +0000

    sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://elormslearningjourney.tech', // Replace this with your actual domain name
+    generateRobotsTxt: true, // This will generate a robots.txt as well
+    // Other configuration options can be added here, if needed.
+  };
+  
